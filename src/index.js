657705cf0070d95b22da5bcc75161fccb7e643a8
Troels Liebe Bentsen <tlb@nversion.dk>
2018-02-15 16:39:06 +0100

    Add missing index so we can install package

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const HttpClient = require('./httpclient')
+const HttpClientError = require('./httpclienterror')
+const HttpClientStream = require('./httpclientstream')
+
+module.exports = {
+  HttpClient,
+  HttpClientError,
+  HttpClientStream
+}
